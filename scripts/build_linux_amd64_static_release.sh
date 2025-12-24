6b9b1cad931daf0d44db1233d386c76afb67fde2
Paweł <screamingbox@gmail.com>
2025-12-24 17:07:12 +0100

    scripts/build_linux_amd64_static_release.sh update

--- scripts/build_linux_amd64_static_release.sh
+++ scripts/build_linux_amd64_static_release.sh
@@ -1,3 +1,2 @@
-cargo build --target=x86_64-unknown-linux-musl --release
-strip target/x86_64-unknown-linux-musl/release/doh
+rustup target add x86_64-unknown-linux-musl && cargo build --target=x86_64-unknown-linux-musl --release && strip target/x86_64-unknown-linux-musl/release/doh
 
